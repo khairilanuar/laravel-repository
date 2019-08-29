beb18846cf72ef657c3a0f304f3d60c3d544fc7a
Khairil Anuar <khairil@gmail.com>
2019-08-29 22:42:06 +0800

    Laravel 6.0 compatibility

--- src/resources/config/repository.php
+++ src/resources/config/repository.php
@@ -231,8 +231,8 @@ return [
         'stubsOverridePath' => app()->path(),
         'paths'         => [
             'models'       => 'Entities',
-            'repositories' => 'Repositories',
-            'interfaces'   => 'Repositories',
+            'repositories' => 'Repositories\\Eloquent',
+            'interfaces'   => 'Contracts\\Repositories',
             'transformers' => 'Transformers',
             'presenters'   => 'Presenters',
             'validators'   => 'Validators',
